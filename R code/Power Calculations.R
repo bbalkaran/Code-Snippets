1958fd74b08723cd8410890477f61c8d8cb6d903
Bridget Balkaran <bbalkaran@aol.com>
2017-12-15 20:54:10 -0700

    power calculation snippet and ReadME
    power calculaiton in R and changes to ReadME

---
+++ R code/Power Calculations.R
@@ -0,0 +1,19 @@
+# Bridget Balkaran 
+# BIOS 6621 Statistical Consulting I 
+# Final: sample size calculation
+#-----------------------------------
+
+power.t.test(n = NULL, delta  = 8, 
+             sd = 17.596, 
+             sig.level = 0.05, 
+             power  = 0.9, 
+             type = c("two.sample"), 
+             alternative = c("two.sided"))
+
+power.t.test(n = NULL, 
+             delta  = 8, 
+             sd = 23.5, 
+             sig.level = 0.05, 
+             power  = 0.9, 
+             type = c("two.sample"), 
+             alternative = c("two.sided"))
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Code Snippets
+
+This repository included snippets of code to refresh my memory or a quick reference if I need to look up syntax I have used before. Feel free to browse!
